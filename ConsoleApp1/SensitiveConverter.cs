03f4976f7accea86328bd3d016af745d8a892a85
Damian Hickey <dhickey@gmail.com>
2020-04-26 20:12:30 +0200

    😎

---
+++ ConsoleApp1/SensitiveConverter.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Newtonsoft.Json;
+
+namespace ConsoleApp1
+{
+    /// <summary>
+    /// Fields with this attribute will have their contents redacted when serialized.
+    /// </summary>
+    public class SensitiveConverter : JsonConverter
+    {
+        public override void WriteJson(JsonWriter writer, object value, JsonSerializer serializer)
+        {
+            writer.WriteValue("**Redacted**");
+        }
+
+        public override object ReadJson(JsonReader reader, Type objectType, object existingValue, JsonSerializer serializer)
+            => throw new NotSupportedException("Should not be attempting to deserialize as sensitive data has been discarded.");
+
+        public override bool CanConvert(Type objectType)
+            => false;
+    }
+}
